1b5c9f289c7a44fb14802e460a5cf988d9c50d1f
rob1997 <robelgetnetrg@gmail.com>
2022-05-10 11:29:32 +0300

    Added Dict Base Editor in Core (Readonly) - Allows for easier Readability Re-usability and Maintainability

--- Editor/DamagableEditor.cs
+++ Editor/DamagableEditor.cs
@@ -2,16 +2,15 @@ using System.Collections.Generic;
 using System.Linq;
 using Core.Utils;
 using Damage;
+using Editor.Core;
 using UnityEditor;
 using UnityEngine;
 
 namespace Editor.Damage
 {
     [CustomEditor(typeof(Damagable))]
-    public class DamagableEditor : UnityEditor.Editor
+    public class DamagableEditor : DictBaseEditor<global::Damage.Damage.DamageType, Damagable.Resistance>
     {
-        private bool _resistanceFoldOut;
-
         public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
@@ -20,46 +19,17 @@ namespace Editor.Damage
         
             if (target == null) return;
 
-            _resistanceFoldOut = EditorGUILayout.Foldout(_resistanceFoldOut, nameof(Damagable.Resistance));
-
-            if (!_resistanceFoldOut) return;
-
-            int margin = 10;
-        
-            GUIStyle marginStyle = new GUIStyle(GUI.skin.label) {margin = new RectOffset(margin, margin, 0, 0)};
-            // do whatever you want with this style, e.g.:
-
-            EditorGUILayout.BeginVertical(marginStyle);
-
-            SerializedProperty pairListProperty = serializedObject.FindProperty("resistance").FindPropertyRelative("serializedList");
-
-            foreach (SerializedProperty property in pairListProperty)
-            {
-                SerializedProperty keyProperty = property.FindPropertyRelative("Key");
-
-                if (keyProperty.enumValueIndex == -1) continue;
-
-                EditorGUILayout.BeginVertical("box");
+            SerializedProperty dictProperty = serializedObject.FindProperty("resistance");
             
-                EditorGUILayout.LabelField(Utils.GetDisplayName($"{(global::Damage.Damage.DamageType) keyProperty.enumValueIndex}"), EditorStyles.boldLabel);
-            
-                SerializedProperty valueProperty = property.FindPropertyRelative("Value");
-            
-                EditorGUILayout.PropertyField(valueProperty.FindPropertyRelative(nameof(Damagable.Resistance.invulnerable)));
-                EditorGUILayout.PropertyField(valueProperty.FindPropertyRelative(nameof(Damagable.Resistance.value)));
-            
-                EditorGUILayout.EndVertical();
-            }
-            
-            List<global::Damage.Damage.DamageType> damageTypes = Utils.GetEnumValues<global::Damage.Damage.DamageType>().ToList();
-            
-            damageTypes.ForEach(d => { if (!damagable.resistance.ContainsKey(d)) damagable.resistance.Add(d, new Damagable.Resistance()); });
-        
-            damagable.resistance.RemoveAll(pair => !damageTypes.Exists(d => d == pair.Key));
-        
-            EditorGUILayout.EndVertical();
+            DrawDict(dictProperty, damagable.resistance);
 
             serializedObject.ApplyModifiedProperties();
+        }
+
+        protected override void DrawValue(SerializedProperty valueProperty)
+        {
+            EditorGUILayout.PropertyField(valueProperty.FindPropertyRelative(nameof(Damagable.Resistance.invulnerable)));
+            EditorGUILayout.PropertyField(valueProperty.FindPropertyRelative(nameof(Damagable.Resistance.value)));
         }
     }
 }
